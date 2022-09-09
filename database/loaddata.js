b5862cfa0badcfbbdc9112470cbc24eefeb6cb0d
Steven Moody <smoody07@gmail.com>
2022-09-09 15:23:11 -0700

    MVP styling

--- database/loaddata.js
+++ database/loaddata.js
@@ -797,16 +797,6 @@ async function loadData() {
 }
 
 loadData();
-// async function getIntentions() {
-//   console.log("getting intentions")
-//   await mongoose.connect('mongodb://localhost:27017/stanislavski');
-//   console.log("db connected")
-//   const intentions = await Intention.find();
-//   console.log(`intentions found. Count: ${intentions.length}`);
-//   console.log(`in db type of intentions: ${typeof intentions}`)
-//   return intentions;
-// }
-
 
 
 module.exports.default = loadData;
